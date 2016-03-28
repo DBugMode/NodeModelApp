8d3ecd4a88981b04ac5567e0c5ed3ca84fe9e545
DBugMode <mitmash@yahoo.com>
2016-03-28 07:24:00 -0400

    Model Init

---
+++ NodeModelApp/Schema/User.js
@@ -0,0 +1,19 @@
+﻿var mongoose = require('mongoose');
+
+var schema = mongoose.Schema;
+
+var userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    address: {
+        street: String,
+        city: String,
+        state: String,
+        zip: String
+    },
+    income: Number
+});
+
+var user = mongoose.Model('User', userSchema);
+
+module.exports(user);
---
+++ NodeModelApp/app.js
@@ -0,0 +1,22 @@
+﻿console.log('Hello world');
+
+var user = require('./Schema/User.js');
+
+var newUser = user({
+    name: 'Chris',
+    age: 35,
+    address: {
+        street: '54 Mango Street',
+        city: 'Boston',
+        state: 'MA',
+        zip: '01233'
+    },
+    income: 98444.34
+    
+});
+
+newuser.save(function (err){
+    if (err) throw err;
+    console.log('Data Saved');
+})
+
